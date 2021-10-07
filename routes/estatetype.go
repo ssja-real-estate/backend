5cbd034c12463652c39bc5056781519937271f7d
amanc1361 <rahymrm@gmail.com>
2021-10-07 17:43:48 +0330

    change estattype router

--- routes/estatetype.go
+++ routes/estatetype.go
@@ -18,7 +18,7 @@ func (r *estatetyperoutes) Install(app *fiber.App) {
 	app.Post("/estatetype", AuthRequired, r.estatetypecontroller.CreateEstateType)
 	app.Put("/estatetype", AuthRequired, r.estatetypecontroller.UpdateEstateType)
 	app.Get("/estatetype/:id", AuthRequired, r.estatetypecontroller.GetEstateType)
-	app.Get("/estatetypes", AuthRequired, r.estatetypecontroller.GetEsatteTypes)
+	app.Get("/estatetype", AuthRequired, r.estatetypecontroller.GetEsatteTypes)
 	app.Delete("/estatetype/:id", AuthRequired, r.estatetypecontroller.DeleteEstateType)
 
 }
