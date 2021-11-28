b44f478b5ee3e1d5db347109711a72dda187cc57
amanc1361 <rahymrm@gmail.com>
2021-11-28 03:52:42 +0330

    fix error in signin

--- controllers/auth.go
+++ controllers/auth.go
@@ -177,16 +177,17 @@ func (c *authController) SignUp(ctx *fiber.Ctx) error {
 func (c *authController) SignIn(ctx *fiber.Ctx) error {
 
 	var input models.User
+	var user *models.User
 	err := ctx.BodyParser(&input)
-
+    
 	if err != nil {
 		return ctx.
 			Status(http.StatusUnprocessableEntity).
 			JSON(util.NewJError(err))
 	}
-
-	user, err := c.usersRepo.GetByMobile(input.Mobile)
-
+    
+	user , err = c.usersRepo.GetByMobile(input.Mobile)
+   
 	if !user.Verify {
 		return ctx.
 			Status(http.StatusUnauthorized).
