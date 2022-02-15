89976ce890886a4563327b3cbe2ae1c4fcacb6da
amanc1361 <rahymrm@gmail.com>
2022-02-15 22:23:22 +0330

    add estate images

--- controllers/estate.go
+++ controllers/estate.go
@@ -1,8 +1,9 @@
 package controllers
 
 import (
+	"fmt"
 	"net/http"
-	"os"
+
 	"realstate/models"
 	"realstate/repository"
 	"realstate/security"
@@ -26,32 +27,36 @@ type estateController struct {
 func NewEstateController(estaterepo repository.EstateRepository) EstateController {
 	return &estateController{estaterepo}
 }
-
 func (r *estateController) CreateEstate(ctx *fiber.Ctx) error {
-
 	var estate models.Estate
-
 	err := ctx.BodyParser(&estate)
-
 	if err != nil {
-
 		return ctx.Status(http.StatusBadRequest).JSON(util.NewJError(err))
 	}
 	userId, err := security.GetUserByToken(ctx)
 	if err != nil {
 		return ctx.Status(http.StatusBadRequest).JSON(util.NewJError(err))
 	}
-	wd, _ := os.Getwd()
+	form, err := ctx.MultipartForm()
 
-	file1, err1 := ctx.FormFile("image")
-	if err1 != nil {
-		return ctx.Status(http.StatusBadRequest).JSON(util.NewJError(err))
+	forms := form.File["image"]
+
+	for _, item := range forms {
+		fmt.Println(item.Filename)
+
 	}
 
-	err = ctx.SaveFile(file1, wd+"/app/images/"+file1.Filename)
-	if err != nil {
-		return ctx.Status(http.StatusBadRequest).JSON(util.NewJError(err))
-	}
+	// wd, _ := os.Getwd()
+	// file1, err1 := ctx.FormFile("image")
+
+	// if err1 != nil {
+	// 	return ctx.Status(http.StatusBadRequest).JSON(util.NewJError(err))
+	// }
+
+	// err = ctx.SaveFile(file1, wd+"/app/images/"+file1.Filename)
+	// if err != nil {
+	// 	return ctx.Status(http.StatusBadRequest).JSON(util.NewJError(err))
+	// }
 	estate.Id = primitive.NewObjectID()
 	estate.UserId = userId
 	estate.Verified = false
@@ -61,13 +66,11 @@ func (r *estateController) CreateEstate(ctx *fiber.Ctx) error {
 	if err != nil {
 		return ctx.Status(http.StatusBadRequest).JSON(util.NewJError(err))
 	}
-
 	err = r.estate.SaveEstate(&estate)
 	if err != nil {
 		return ctx.Status(http.StatusBadRequest).JSON(util.NewJError(err))
 	}
-	return ctx.Status(http.StatusOK).JSON(fiber.Map{"data": "Estate Created...."})
-
+	return ctx.Status(http.StatusOK).JSON(estate)
 }
 
 func (r *estateController) GetEstate(ctx *fiber.Ctx) error {
