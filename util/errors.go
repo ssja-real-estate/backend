4f866e63307aedcb2dc6cc17575fa81ee5e06bea
amanc1361 <rahymrm@gmail.com>
2023-02-04 17:36:27 +0330

    errors update

--- util/errors.go
+++ util/errors.go
@@ -41,5 +41,5 @@ var (
 	SuccessDelete         = "گزینه مورد نظر با موفقیت حذف شد"
 	SuccessUpdate         = "آیتم مورد نظر با موفقیت اپدیت شد."
 	SuccessSendSms        = "کد فعال سازی با موفقیت ارسال شد"
-	SuccessChangePassword = "رمز عبور شما با موفیقت بروزرسانی شد "
+	SuccessChangePassword = "رمز عبور شما با موفیقت بروزرسانی شد"
 )
