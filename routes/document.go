eee41dec88f66fb21bb6467fb4d36646a9bd17ae
amanc1361 <rahymrm@gmail.com>
2023-12-19 08:37:56 +0330

    update doc

--- routes/document.go
+++ routes/document.go
@@ -18,7 +18,6 @@ func (r *documentRoute) Install(app *fiber.App) {
 	api := app.Group("/api")
 	api.Post("/document", AuthRequired, r.documentController.CreateDocument)
 	api.Get("/document/:id", AuthRequired, r.documentController.GetDoucmentById)
-
 	api.Delete("/document/:documentId", AuthRequired, r.documentController.DeleteDocument)
 	api.Get("/document", AuthRequired, r.documentController.GetDocuments)
 
