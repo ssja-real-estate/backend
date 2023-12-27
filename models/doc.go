6afbfc5cb4d893aa8ea72468793096c00e3ce64b
amanc1361 <rahymrm@gmail.com>
2023-12-27 15:46:55 +0330

    change document model

--- models/doc.go
+++ models/doc.go
@@ -6,4 +6,5 @@ type Document struct {
 	Id    primitive.ObjectID `json:"id" bson:"_id"`
 	Title string             `json:"title" `
 	Path  string             `json:"path"`
+	Type  int                `json:"type"`
 }
