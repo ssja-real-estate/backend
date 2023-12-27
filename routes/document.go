cdfb01d23c52efe2eda645c19ea3d1c94ad667bc
amanc1361 <rahymrm@gmail.com>
2023-12-27 12:23:54 +0330

    remove auth in get doc

--- routes/document.go
+++ routes/document.go
@@ -17,7 +17,7 @@ func NewDocumentRoute(documentController controllers.DocumentController) Routes 
 func (r *documentRoute) Install(app *fiber.App) {
 	api := app.Group("/api")
 	api.Post("/document", AuthRequired, r.documentController.CreateDocument)
-	api.Get("/document/:id", AuthRequired, r.documentController.GetDoucmentById)
+	api.Get("/document/:id", r.documentController.GetDoucmentById)
 	api.Delete("/document/:documentId", AuthRequired, r.documentController.DeleteDocument)
 	api.Get("/document", r.documentController.GetDocuments)
 	api.Put("/document", AuthRequired, r.documentController.CreateDocument)
